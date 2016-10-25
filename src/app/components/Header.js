28801a94512fc233091ab42da7b215c9c664c9ff
Alexey Molchan <alexeymolchan228@gmail.com>
2016-10-25 20:09:53 +0300

    Added 2 way data-binding for input field

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default class Header extends React.Component{
-    constructor(props){
-        super();
-        this.state = {name: 'REACT is awesome'};
-        this.props = props;
+    handleChange(e){
+        const name = e.target.value;
+        this.props.changeName(name);
     }
     render(){
         return(
             <div>
-                <h1>{this.props.hi}, {this.state.name} !</h1>
+                <h1>{this.props.hi} {this.props.name}</h1>
+                <input type='text' value={this.props.name} onChange={this.handleChange.bind(this)} onBlur={(e) => e.target.value = ''}/>
             </div>
         )
     }
