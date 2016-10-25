2ec35fb4dea0442a43aa84c5b9d75d485cc9aa30
Alexey Molchan <alexeymolchan228@gmail.com>
2016-10-25 14:03:02 +0300

    Using props and states

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 export default class Header extends React.Component{
+    constructor(props){
+        super();
+        this.state = {name: 'REACT is awesome'};
+        this.props = props;
+    }
     render(){
         return(
             <div>
-                <h1>What's up guys, header is coming! xD</h1>
+                <h1>{this.props.hi}, {this.state.name} !</h1>
             </div>
         )
     }
