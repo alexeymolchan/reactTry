628415c4ce66ee0d6ecddd2790be69fdc5eecffe
Alexey Molchan <alexeymolchan228@gmail.com>
2016-11-20 00:01:03 +0300

    adding object spread plugin

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
                 include: path.resolve(__dirname, 'src'),
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'es2015'],
+                    plugins: ['transform-object-rest-spread']
                 }
             },
             {
