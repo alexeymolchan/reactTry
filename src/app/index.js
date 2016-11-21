0a015d53a761ebfb33eb87eb55605a4ce44efcc3
Alexey Molchan <alexeymolchan228@gmail.com>
2016-11-21 23:21:21 +0300

    array destructuring/output divs via map function

--- src/app/index.js
+++ src/app/index.js
@@ -1,29 +1,39 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import './index.css'
 
 class App extends React.Component{
-    constructor(){
-        super();
-        this.state = {name: 'REACT is awesome'};
+    constructor(props){
+        super(props);
+        this.state = {
+            arr: [1,2,3]
+        }
     }
 
-    changeName(name){
-        this.setState({name});
+    changeName(){
+        this.setState({
+            arr: [...this.state.arr, 4]
+        });
     }
     render(){
+        console.log(this.state.arr);
+        let numbers;
+        if (this.state.arr.length > 3){
+            numbers = <div>{this.state.arr.map((numb, index) => <div key={index}>{numb}</div>)}</div>
+        }
         return(
             <div className="appDiv">
-                <Header changeName={this.changeName.bind(this)} hi="Hi dudes!" name={this.state.name}/>
+                <button onClick={this.changeName.bind(this)}>push</button>
+                {numbers}
             </div>
         )
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));*/
+ReactDOM.render(<App/>, document.getElementById('app'));
 
-import { createStore} from 'redux';
+/*import { createStore} from 'redux';
 
 const initialState = {
     result: 0,
@@ -68,4 +78,4 @@ store.dispatch({type: "SUBTRACT", payload: 75});
 
 store.dispatch({type: "ADD_SONG", payload: 'Tumblr girls'});
 
-store.dispatch({type: "ADD_SONG", payload: 'Make me move'});
+store.dispatch({type: "ADD_SONG", payload: 'Make me move'});*/
